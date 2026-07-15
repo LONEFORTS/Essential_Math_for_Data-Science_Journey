2e8effaec30b1bf3eb187354ad690ac13cfd610d
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-15 18:45:55 +0530

    Create 01_order_of-operatons.py

---
+++ 01_BASIC-MATHS/01_order_of-operatons.py
@@ -0,0 +1,31 @@
+
+#date-Wed-15-July-2026
+ 
+#HelloWorld this is my first program of 01_BASIC_MATHS of order of operations For essential math for data sciecne of this journey.
+
+#What is Order of Operations: 
+#Order of Operations means an order by which We solve the mathematical expression. 
+#such as PEMDAS by which i am going to solve this:  
+
+
+# 2 x (3+2)^2 / 5 -4 
+
+# here'a how it is expressed in code. 
+
+my_value = 2 * (3+2)**2+-4 
+print(my_value) 
+
+
+#or to here is the same example i used parentheses helping set the apart from the rest of the expression. 
+
+
+my_values = 2*((3+2)**2+5)-4
+print(my_values) 
+#prints 6.0
+
+
+
+
+
+#here is what happens here : 
+#1Firstly the compontenxts of parantheses later Exponents are evaluated,later the MUL, Div, and next is addition and subtraction. 
