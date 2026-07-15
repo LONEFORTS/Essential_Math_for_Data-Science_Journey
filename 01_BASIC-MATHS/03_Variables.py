3ba0ff014060b1fd6f01aa9b71893188ed857970
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-15 19:00:35 +0530

    Create 03_Variables.py

---
+++ 01_BASIC-MATHS/03_Variables.py
@@ -0,0 +1,11 @@
+#Date-Wed-15-July-2026
+#Thia ia my Third program about Variables.
+#variables are placeholders in mathematics as in programming.
+#Such as we can represent x as reperesnting a Real Numbers without declaring what is it.
+
+#here in this example I can take input x from a user and multiply it by 3. 
+
+x = int(input("Input a Number:\n")
+product = 3*x 
+print(product)
+
