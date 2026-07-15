4ee41c806e144cb9773c6c5fef6a8b5a2773d958
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-15 18:49:47 +0530

    Update 01_order_of-operatons.py

--- 01_BASIC-MATHS/01_order_of-operatons.py
+++ 01_BASIC-MATHS/01_order_of-operatons.py
@@ -16,12 +16,6 @@ my_value = 2 * (3+2)**2+-4
 print(my_value) 
 
 
-#or to here is the same example i used parentheses helping set the apart from the rest of the expression. 
-
-
-my_values = 2*((3+2)**2+5)-4
-print(my_values) 
-#prints 6.0
 
 
 
