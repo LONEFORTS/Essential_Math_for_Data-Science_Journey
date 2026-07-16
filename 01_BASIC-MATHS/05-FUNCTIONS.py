b20d7e102b7239a122b62e3bb05877b978a3f72e
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-16 15:54:03 +0530

    Create 05-FUNCTIONS.py

---
+++ 01_BASIC-MATHS/05-FUNCTIONS.py
@@ -0,0 +1,21 @@
+#3;46
+#date:Thursday.16-july-2026
+#This is my fifth program about functions.
+@Function in mathematics define the relationship between two or more variables there ia an independent variable and an dependent variable.for in example in this y=2x+1 here y is the dependent variable and x is inde[enednt variable. 
+#functions are useful because they predict the relationship of variables of models. 
+
+#Function can be expessed such as y=2x+1 or we can also use another covention Instead of expressing y=2x+1 we can explcitly label y as function of x.
+
+
+
+#declaring a linear function in python: 
+
+
+def f(x): 
+    return 2*x+1
+
+x_values = [0,1,2,3] 
+
+for x in x_values: 
+    y=f(x)
+    print(y) 
