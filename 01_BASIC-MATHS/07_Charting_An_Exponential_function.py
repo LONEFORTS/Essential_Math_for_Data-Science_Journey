4fd6b264760bb8cdce0cbc3e37344ce5afe61056
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-18 21:13:54 +0530

    Create 07_Charting_An_Exponential_function.py

---
+++ 01_BASIC-MATHS/07_Charting_An_Exponential_function.py
@@ -0,0 +1,13 @@
+#HelloWOrld,9;10pm
+#date: Saturday 18-July:2026
+
+#here this is the program to show graph of Exponential function, here the output is shown as parabola becuase the equation i hae used here is not linear function it is a curvilinear function.
+
+#defintion of curvilinear function can be define as when a function produces a curvy and smooth line rather than a straight line, it is known as "Curvilinear function"
+
+#here is the program about it: 
+
+from sympy import * 
+x=symbols("x") 
+f = x**2+1 
+plot(f)
