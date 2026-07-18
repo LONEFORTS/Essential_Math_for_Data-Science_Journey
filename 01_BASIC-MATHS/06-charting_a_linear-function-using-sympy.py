23947f748378d4940eeff5ed5c5e96b9eb4809f3
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-18 14:24:14 +0530

    Create 06-charting_a_linear-function-using-sympy.py

---
+++ 01_BASIC-MATHS/06-charting_a_linear-function-using-sympy.py
@@ -0,0 +1,10 @@
+#HelloWorld Good Afternoon 
+#2.19pm ; date ; saturday 18-july-2026 
+
+#this is  program about using sympy here i am using this python library to plot the function and get the graph.  
+
+
+from sympy impport * 
+x = symbols('x') 
+f = 2 * x + 1 
+plot(f) 
