ebc29654bb8a46d49a65f524727cd5e57f4e85b5
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-19 22:55:19 +0530

    Create 10-Summation-of-elements.py

---
+++ 01_BASIC-MATHS/10-Summation-of-elements.py
@@ -0,0 +1,11 @@
+#HelloWOrld 10.50 
+#date.SUNDAY.19-July-2026
+
+#it is also common to see the 'n' to reperesnt the collection of items just as the number of records in dataset. 
+
+
+x = [1,4,6,2]
+n = len(x) 
+summation = sum(10*x[i] for i in range(0,n)) 
+
+printt(summaton)
