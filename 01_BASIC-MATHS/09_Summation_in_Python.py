759fcd51b100b310c4a7b7dbb730194ac9c756b4
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-19 22:50:06 +0530

    Create 09_Summation_in_Python.py

---
+++ 01_BASIC-MATHS/09_Summation_in_Python.py
@@ -0,0 +1,9 @@
+#HelloWorld 10.46
+#Date.Sunday 19-July-2026
+
+#Today, this programme is about the summation in mathematics and python.
+
+#A summation is expressed as sigma symbol in python and used to add elements in python. 
+
+summation = sum(2*i for i in range(0,6))
+print(summation)
