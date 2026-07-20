9e4020e6d6e64532e85e8c019e7fc07efb0e29c4
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-20 20:57:30 +0530

    Create 11-SUMMATON-IN-SYMPY.py

---
+++ 01_BASIC-MATHS/11-SUMMATON-IN-SYMPY.py
@@ -0,0 +1,15 @@
+#HelloWOrld.8.56pm
+#date.Monday.20July2026
+
+#this programme is about summation using sympy.
+
+
+from sympy import *
+i,n = symbols('i n')
+# iterate each element i from 1 to n,
+# then multiply and sum
+summation = Sum(2*i,(i,1,n))
+# specify n as 5,
+# iterating the numbers 1 through 5
+up_to_5 = summation.subs(n, 5)
+print(up_to_5.doit()) # 30
