9f8a3b0b3201e092928e3c72ab211abd2db5e069
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-21 15:49:30 +0530

    Create 13_Logaraithm(Log())_and_about.py

---
+++ 01_BASIC-MATHS/13_Logaraithm(Log())_and_about.py
@@ -0,0 +1,14 @@
+#HEllOWORLD 3.45pm
+#date.TUesday.2026.21.july
+
+#helloworld, today this programme is about the Logarithm in maths, althougt.
+#Logarithm can be defined as  is a math function used to find power for a specific number and base.information and representation of it/will upload on notes after completion. 
+
+#also logarithm is key part of logistic regression and also used in earth quakes measuing, sound and more. 
+
+#here is the program of logarithm. 
+
+
+from math import log * 
+x= log(8,2) 
+print(x)
