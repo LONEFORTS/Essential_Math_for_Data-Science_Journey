526416beb66d58c95309138f1ebff7343ea82731
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-22 00:24:31 +0530

    Create 16-naturalLogarithm.py

---
+++ 01_BASIC-MATHS/16-naturalLogarithm.py
@@ -0,0 +1,15 @@
+#helloworld 12.20 
+#date.22.july.2026.wednesday 
+
+#this is about the natural logarithms 
+#Well..when we use "e" as our base it is known as natural algorithkm. 
+#Depending on the platoform we may use In() instead of log() to specify a natural logarith in our programe. 
+
+#However in python, a natural logarithm is specified by the log() 
+# it is becuade the default is the base for the log() function is "e" in python we need to just to leave the arugment for the base enitrely  and it wil default to using "e" as the base. 
+
+#here is the programe for it. 
+
+from maht improt log 
+x = log(10) 
+print(x) 
