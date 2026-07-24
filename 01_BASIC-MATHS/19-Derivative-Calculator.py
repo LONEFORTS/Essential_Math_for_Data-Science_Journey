77b8047a45233b5148ddf5ac2906e8b29f69f779
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-25 00:56:11 +0530

    Update 19-Derivative-Calculator.py

--- 01_BASIC-MATHS/19-Derivative-Calculator.py
+++ 01_BASIC-MATHS/19-Derivative-Calculator.py
@@ -4,4 +4,14 @@
 #About Derivatives : 
 
 #A derivative tells us the slope of a given function, and it is useful in machine learning and other mathematical algorithms.
-#ANd useful with gradient descent 
+#ANd useful with gradient descent  
+
+def derivative_x(f,x,step_size): 
+    m= (f(x+step_size) - f(x) / ( ( x+ step_size)-x) 
+    return m 
+
+def my_function(x): 
+    return x**2 
+
+step_at_2 = derivative_x(my_function,2,.00001)
+print(step_at_2) 
