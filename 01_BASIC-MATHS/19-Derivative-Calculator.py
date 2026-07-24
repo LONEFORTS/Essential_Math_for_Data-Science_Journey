13e4c5c33c878b3383e942c77bdb43de7f972bf1
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-24 23:59:16 +0530

    Create 19-Derivative-Calculator.py

---
+++ 01_BASIC-MATHS/19-Derivative-Calculator.py
@@ -0,0 +1,7 @@
+#HElloworld 11.56am 
+#date.24JULY2026 
+
+#About Derivatives : 
+
+#A derivative tells us the slope of a given function, and it is useful in machine learning and other mathematical algorithms.
+#ANd useful with gradient descent 
