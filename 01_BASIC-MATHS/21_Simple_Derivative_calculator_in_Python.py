ba159b2db41cfdee9feb46541e6e8cd959226bd3
Ayush Sudhir Lone <lonesforts@gmail.com>
2026-07-31 20:00:07 +0530

    Create 21_Simple_Derivative_calculator_in_Python.py

---
+++ 01_BASIC-MATHS/21_Simple_Derivative_calculator_in_Python.py
@@ -0,0 +1,10 @@
+#HelloWorld 7.59PM
+#DATE : 31FRI2026/JULY.
+
+#THIS PROGRAM CALCULATES DERIVATIVE SIMPLY 
+def f(x):
+ return x**2
+def dx_f(x):
+ return 2*x
+slope_at_2 = dx_f(2.0)
+print(slope_at_2) # prints 4.0
